fe322374f42c478b653254942217df372990ec46
Bojan Kalicanin <kalcho@zoho.com>
2016-12-29 21:31:01 +0100

    added solution to exercise 18-2

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -21,7 +21,18 @@ class Entry(models.Model):
     
     class Meta:
         verbose_name_plural = 'entries'
-    
+
+# 18-2. Short Entries: The __str__() method in the Entry model currently
+# appends an ellipsis to every instance of Entry when Django shows it in
+# the admin site or the shell. Add an if statement to the __str__()
+# method that adds an ellipsis only if the entry is more than 50
+# characters long. Use the admin site to add an entry that's fewer than
+# 50 characters in length, and check that it doesn’t have an ellipsis
+# when viewed.
+
     def __str__(self):
         """Return a string representation of the model."""
-        return self.text[:50] + '...'
+        if len(self.text) < 50:
+            return self.text
+        else:
+            return self.text[:50] + '...'
