83b0a824723b8a8e495a93ee39cfc2c3a4fe80c3
Bojan Kalicanin <kalcho@zoho.com>
2016-12-29 10:23:03 +0100

    added initial venv and django files

---
+++ ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\users\debkal\downloads\python\learning_log\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
