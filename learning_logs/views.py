3e347dac711dd9620408828fd2bd7da94d76d75c
Bojan Kalicanin <kalcho@zoho.com>
2017-01-09 23:26:20 +0100

    added get_object_or_404 on views

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -51,7 +51,7 @@ def new_topic(request):
 @login_required
 def new_entry(request, topic_id):
     """Add a new entry."""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     check_topic_owner(request, topic)
     
     if request.method != 'POST':
@@ -73,7 +73,7 @@ def new_entry(request, topic_id):
 @login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
-    entry = Entry.objects.get(id=entry_id)
+    entry = get_object_or_404(Entry, id=entry_id)
     topic = entry.topic
     check_topic_owner(request, topic)
 
